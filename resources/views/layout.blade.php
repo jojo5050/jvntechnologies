295a3cf764e3898e5c7f79147dc3daa7ec527e64
darl <darlcloudprojects@gmail.com>
2026-02-03 07:36:59 +0100

    vite fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>JVN Techologies</title>
+        <title>JVN Technologies</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Fonts -->
