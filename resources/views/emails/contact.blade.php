58a996a5cca4d8d78c91091afc912989fa74b21e
darl <darlcloudprojects@gmail.com>
2026-02-09 12:35:17 +0100

    mail settup2

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,29 +1,8 @@
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="A leading software development company in Nigeria. Headquartered in the heart of the Federal Capital Territory, Abuja">
-
-        <title>JVN Technologies</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        
-    </head>
-
-
-
-         <body>
-
             <h2>New Message from JVN Technologies Website</h2>
             <p><strong>Name:</strong> {{ $details['name'] }}</p>
             <p><strong>Email:</strong> {{ $details['email'] }}</p>
             <p><strong>Subject:</strong> {{ $details['subject'] }}</p>
             <p><strong>Message:</strong></p>
             <p>{{ $details['message'] }}</p>
-        </body>    
-</html>
+  
